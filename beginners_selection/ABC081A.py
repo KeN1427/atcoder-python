371323da9356d2f11fc4b39773d8b4cc91bba4c7
KeN <ken.t.csai@gmail.com>
2021-10-02 01:12:54 +0900

    create: ABC081A.py

---
+++ beginners_selection/ABC081A.py
@@ -0,0 +1,13 @@
+"""
+ABC081A - Placing Marbles
+
+reference: https://atcoder.jp/contests/abs/tasks/abc081_a
+"""
+
+lst = list(map(int, input()))
+count = 0
+for i in lst:
+	if i == 1:
+		count += 1
+
+print(count)
