bc43228497c67bea317ffb7827e08542f93c08be
KeN <ken.t.csai@gmail.com>
2021-10-02 01:11:39 +0900

    create: PracticeA.py

---
+++ beginners_selection/PracticeA.py
@@ -0,0 +1,11 @@
+"""
+PracticeA - Welcome to AtCoder
+
+reference: https://atcoder.jp/contests/abs/tasks/practice_1
+"""
+
+a = int(input())
+b, c = map(int, input().split())
+str = input()
+
+print(f'{a + b + c} {str}')
