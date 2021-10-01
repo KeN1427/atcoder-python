e039c6e20766335bfbfbe0c068cf07953a872153
KeN <ken.t.csai@gmail.com>
2021-10-02 01:12:16 +0900

    create: ABC086.py

---
+++ beginners_selection/ABC086A.py
@@ -0,0 +1,12 @@
+"""
+ABC086A - Product
+
+reference: https://atcoder.jp/contests/abs/tasks/abc086_a
+"""
+
+a, b = map(int, input().split())
+
+if a * b % 2 == 0:
+	print('Even')
+else:
+	print('Odd')
