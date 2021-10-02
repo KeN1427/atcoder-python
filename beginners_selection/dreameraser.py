406da055492b16ea8fb3669076e6beddb4f26c91
KeN <ken.t.csai@gmail.com>
2021-10-03 01:07:34 +0900

    create: dreameraser.py

---
+++ beginners_selection/dreameraser.py
@@ -0,0 +1,11 @@
+"""
+ABC049C - 白昼夢
+
+problem: https://atcoder.jp/contests/abs/tasks/arc065_a
+"""
+
+s = input().replace('eraser', '').replace('erase', '').replace('dreamer', '').replace('dream', '')
+if s:
+	print('NO')
+else:
+	print('YES')
