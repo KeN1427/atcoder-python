09065199431f9a8b59d9620246817b67e3010700
KeN <ken.t.csai@gmail.com>
2021-10-02 13:15:20 +0900

    create: kagami_mochi.py

---
+++ beginners_selection/kagami_mochi.py
@@ -0,0 +1,12 @@
+"""
+ABC085B - Kagami Mochi
+
+problem: https://atcoder.jp/contests/abs/tasks/abc085_b
+"""
+
+n = int(input())
+lst = []
+for i in range(n):
+	lst.append(int(input()))
+
+print(len(set(lst)))
