cf798c6e0987e44a9e4c95b245ec60daa1663dbc
KeN <ken.t.csai@gmail.com>
2021-10-03 01:06:57 +0900

    create: otoshidama.py

---
+++ beginners_selection/otoshidama.py
@@ -0,0 +1,15 @@
+"""
+ABC085C - Otoshidama
+
+problem: https://atcoder.jp/contests/abs/tasks/abc085_c
+"""
+n, y = map(int, input().split())
+
+for i in range(n + 1):
+	for j in range(n + 1 - i):
+		k = (y - (i * 10000 + j * 5000))//1000 
+		if i + j + k == n:
+			print(f'{i} {j} {k}')
+			exit()
+else:
+	print('-1 -1 -1')
