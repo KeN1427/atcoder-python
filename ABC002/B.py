21221a9091cc87164bb5dde1c67f6ee721afdfdb
KeN <ken.t.csai@gmail.com>
2021-10-05 00:56:52 +0900

    create: ABC002 - B

---
+++ ABC002/B.py
@@ -0,0 +1,14 @@
+"""
+B -  罠
+
+problem:https://atcoder.jp/contests/abc002/tasks/abc002_2
+"""
+
+W = input()
+
+trap = 'aiueo'
+
+for i in range(len(trap)):
+	W = W.replace(trap[i], '') 
+
+print(W)
