bee71a60d483166b85fc6e0d09d1db4b6f6c002f
KeN <ken.t.csai@gmail.com>
2021-10-08 22:45:02 +0900

    create: ABC002 - D

---
+++ ABC002/D.py
@@ -0,0 +1,36 @@
+"""
+D - 派閥
+
+problem: https://atcoder.jp/contests/abc002/tasks/abc002_4
+"""
+
+import itertools
+
+N, M = map(int, input().split())
+
+relations = [[0] * (N+1) for i in range(N+1)]
+for i in range(M):
+	x, y = map(int, input().split())
+	relations[x][y] = 1
+	relations[y][x] = 1
+
+print(relations)
+result = 0
+for bit in range(2 ** N):
+	group = []
+	for i in range(N):
+		print(bit, i)
+		if (bit >> i) & 1:
+			print('append')
+			group.append(i + 1)
+	flag = 1
+	for j in itertools.combinations(group, 2):
+		if relations[j[0]][j[1]] == 0:
+			print('false')
+			flag = 0
+			break
+	if flag == 1:
+		result = max(result, len(group))
+
+print(result)
+
